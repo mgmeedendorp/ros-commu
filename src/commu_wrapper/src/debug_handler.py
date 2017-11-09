9fca0703881b8b237a31c63cf96f119971780bc0
Maurice Meedendorp <mauricemeedendorp@gmail.com>
2017-11-09 15:00:56 +0900

    Service

--- src/commu_wrapper/src/debug_handler.py
+++ src/commu_wrapper/src/debug_handler.py
@@ -9,17 +9,21 @@ import rospy
 class DebugHandler:
 
     def __init__(self, camera_topic, window_name="Debug window (press ESC to close)"):
+        rospy.loginfo("Initializing DebugHandler..")
         self.window_name = window_name
         self.image_bridge = CvBridge()
 
         self.latest_cv_image = None
         self.thread = None
 
+        rospy.loginfo("Initializing DebugHandler cv2 window with name: '%s'..", window_name)
         cv2.namedWindow(self.window_name, cv2.WINDOW_NORMAL)
 
+        rospy.loginfo("Subscribing to '%s' topic for images..", camera_topic)
         rospy.Subscriber(camera_topic, Image, self.image_received)
 
     def image_received(self, data):
+        rospy.loginfo("Received image.")
         self.latest_cv_image = self.image_bridge.imgmsg_to_cv2(data, "bgr8")
 
     def spin_image_window(self):
@@ -36,6 +40,8 @@ class DebugHandler:
                     cv2.imshow(self.window_name, self.latest_cv_image)
 
                 if cv2.waitKey(1/60) & 0xFF == 27:
+                    rospy.loginfo("Closing window because ESC was pressed")
+                    cv2.destroyWindow(self.window_name)
                     break
 
         self.thread = Thread(target=worker)
