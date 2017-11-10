0f57c03b479b74ad14b42c00ff7b712481e06a10
Maurice Meedendorp <mauricemeedendorp@gmail.com>
2017-11-10 12:34:40 +0900

    Look and utter support in debug window also margins

--- src/commu_wrapper/src/util.py
+++ src/commu_wrapper/src/util.py
@@ -151,7 +151,11 @@ def add_alpha_layer(cv_image, value = 255):
     b, g, r = cv2.split(cv_image)
     alpha = np.full(b.shape, value, dtype=b.dtype)
 
-    return cv2.merge((b, g, r, alpha))
+    merged = cv2.merge((b, g, r, alpha))
+
+    print merged.shape
+
+    return merged
 
 
 def draw_image_margin(cv_image, margin_size=50):
