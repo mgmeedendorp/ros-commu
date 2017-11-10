f6e6f2906f8ec9c3495e2dbdd102859acf47d581
Maurice Meedendorp <mauricemeedendorp@gmail.com>
2017-11-10 10:55:55 +0900

    Look and utter support in debug window also margins

--- src/commu_wrapper/src/debug_handler.py
+++ src/commu_wrapper/src/debug_handler.py
@@ -85,6 +85,7 @@ class DebugHandler:
         def worker():
             rospy.loginfo("Initializing DebugHandler cv2 window with name: '%s'..", self.window_name)
             cv2.namedWindow(self.window_name, cv2.WINDOW_NORMAL)
+            cv2.resizeWindow(self.window_name, 960, 1280)
 
             rospy.loginfo("Starting image worker thread.")
 
