541f3b0de386bcb798360426fbb672bed7a053bc
Maurice Meedendorp <mauricemeedendorp@gmail.com>
2017-11-14 16:59:30 +0900

    Utter text

--- src/commu_wrapper/src/debug_handler.py
+++ src/commu_wrapper/src/debug_handler.py
@@ -51,6 +51,8 @@ class DebugHandler:
         self.latest_classification_image = util.draw_bounding_boxes(cv_image, data.objects, self.image_margin)
 
     def commu_utter_received(self, utterance, blocking, english):
+        print "debug utter"
+
         cv_image = np.zeros(self.image_size, np.uint8)
         cv_image = util.add_alpha_layer(cv_image, 0)
         cv_image = util.draw_image_margin(cv_image, self.image_margin)
