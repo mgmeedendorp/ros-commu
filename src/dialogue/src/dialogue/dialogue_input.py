4a0ee0f1e2683fc0d4e344e5ba21b03df2c86c12
Maurice Meedendorp <mauricemeedendorp@gmail.com>
2017-11-14 15:21:25 +0900

    Imports. again......

--- src/dialogue/src/dialogue/dialogue_input.py
+++ src/dialogue/src/dialogue/dialogue_input.py
@@ -1,7 +1,7 @@
 import time
 import rospy
 from button_input.srv import BinaryButtonInput
-from dialogue.util import get_srv_function
+from src.dialogue.src.util import *
 
 
 class AbstractDialogueResponse:
