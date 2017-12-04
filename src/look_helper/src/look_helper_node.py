5b84f9de870b6bfbf7d9e042b85d78b717b883e7
Maurice Meedendorp <mauricemeedendorp@gmail.com>
2017-12-04 15:20:12 +0900

    Log info

--- src/look_helper/src/look_helper_node.py
+++ src/look_helper/src/look_helper_node.py
@@ -39,13 +39,13 @@ if __name__ == '__main__':
 
     rospy.loginfo("Creating LookManager..")
 
-    t_x = rospy.get_param('tx', 0)
-    t_y = rospy.get_param('ty', 0)
-    t_z = rospy.get_param('tz', 0)
+    t_x = rospy.get_param('~tx', 0)
+    t_y = rospy.get_param('~ty', 0)
+    t_z = rospy.get_param('~tz', 0)
 
-    r_x = rospy.get_param('rx', 0)
-    r_y = rospy.get_param('ry', 0)
-    r_z = rospy.get_param('rz', 0)
+    r_x = rospy.get_param('~rx', 0)
+    r_y = rospy.get_param('~ry', 0)
+    r_z = rospy.get_param('~rz', 0)
 
     rospy.loginfo("LookManager initializing with: {tx: %f, ty: %f, tz: %f, rx: %f, ry: %f, rx: %f}", t_x, t_y, t_z, r_x, r_y, r_z)
 
