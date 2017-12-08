3d042290240598df3a2d148773b2da6181656107
Maurice Meedendorp <mauricemeedendorp@gmail.com>
2017-12-08 16:38:06 +0900

    4 looks per second is too much for poor Sota

--- src/look_helper/src/look_helper_node.py
+++ src/look_helper/src/look_helper_node.py
@@ -7,7 +7,7 @@ import time
 
 
 last_classification_time = 0
-max_looks_per_second = 4
+max_looks_per_second = 3
 
 def person_classification_callback(manager, data):
     # type: (LookManager, PersonDetection) -> None
