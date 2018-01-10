542870c5cfda7eaac48ba5d14775ecaa328b6607
Maurice Meedendorp <3658232+seremis@users.noreply.github.com>
2018-01-10 11:28:45 +0900

    Testing transforms because the rotation function I wrote is not gonna work

--- src/look_helper/src/transform/transform_broadcaster.py
+++ src/look_helper/src/transform/transform_broadcaster.py
@@ -11,6 +11,23 @@ import tf2_ros
 import geometry_msgs.msg
 
 if __name__ == '__main__':
+    tx = float(sys.argv[1])
+    ty = float(sys.argv[2])
+    tz = float(sys.argv[3])
+    rx = float(sys.argv[4])
+    ry = float(sys.argv[5])
+    rz = float(sys.argv[6])
+
+    br = tf.TransformBroadcaster()
+    br.sendTransform(
+        (tx, ty, tz),
+        tf.transformations.quaternion_from_euler(rx, ry, rz),
+        rospy.Time.now(),
+        "camera_link",  # Publish a transform from `camera_link` (provided by euclid)
+        "commu_link"    # to `commu_link` (the origin of the commu coordinate system)
+    )
+
+
     if len(sys.argv) < 8:
         rospy.logerr(
             'Invalid number of parameters\nusage: ./static_turtle_tf2_broadcaster.py child_frame_name x y z roll pitch yaw')
