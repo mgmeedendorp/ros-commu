fc00b689aaacc74e99aa1a6c4e1ca2de679c8fff
Maurice Meedendorp <3658232+seremis@users.noreply.github.com>
2018-01-29 12:32:36 +0900

    Object prioritization now based on size

--- src/dialogue/src/dialogue_node.py
+++ src/dialogue/src/dialogue_node.py
@@ -3,25 +3,36 @@
 import sys
 
 import rospy
-from ssd.msg import ClassifiedObjectArray
+from ssd.msg import ClassifiedObjectArray, ClassifiedObject
 
 from dialogue.dialogue_definition_quiz_objects import DialogueLibraryQuiz
 from dialogue.dialogue_manager import DialogueManager
 
 
 def classification_result_callback(manager, data):
+    # type: (DialogueManager, ClassifiedObjectArray) -> None
     rospy.logdebug("Received classification results with {} objects.".format(len(data.objects)))
 
-    for obj in data.objects:
+    image = data.image
+
+    for obj in data.objects:  # type: ClassifiedObject
         short_term_history = manager.get_topic_history()[-10:]
 
         count = short_term_history.count(obj.label)
 
         if count <= 0:
+
+            area = obj.bbox.x_size * obj.bbox.y_size
+            norm_area = area / (image.width * image.height)
+
             if obj.label == "person":
-                manager.add_topic(obj.label, obj.id, 1)
+                priority = norm_area * 1.5
+                if priority > 1:
+                    priority = 1
+
+                manager.add_topic(obj.label, obj.id, priority)
             else:
-                manager.add_topic(obj.label, obj.id, .9)
+                manager.add_topic(obj.label, obj.id, norm_area)
 
 
 def init_message_listeners(manager):
